c6b4bae900a27ec31b5a55b18518e47e7975a772
IX-0 <igorbaltarejo05@gmail.com>
2023-11-19 15:01:47 +0000

    move function redesign

--- game2048.js
+++ game2048.js
@@ -1,5 +1,5 @@
 var highscore = 0
-var prevGameState
+var prevGameStates = []
 
 function convertCoords(x,y){
     return x.toString() + y.toString()
@@ -15,7 +15,6 @@ function newBlock(value=2,x=0,y=0) {
             blockAt(x,y).innerHTML = '<p class="block">'+ value +'</p>'
             blockAt(x,y).value = value
             blockAt(x,y).style.backgroundColor = "rgb(238,228,218)"
-            
             return
         }
         
@@ -26,44 +25,32 @@ function newBlock(value=2,x=0,y=0) {
         blockAt(x,y).innerHTML = '<p class="block">'+ value +'</p>'
         blockAt(x,y).value = value
 
-        switch (blockAt(x,y).value) {
-            case 2:
-                blockAt(x,y).style.backgroundColor = "rgb(238,228,218)"
+        switch (value) {
+            case 2: blockAt(x,y).style.backgroundColor = "rgb(238,228,218)"; 
                 break;
-            case 4:
-                blockAt(x,y).style.backgroundColor = "rgb(237,224,200)"    
+            case 4: blockAt(x,y).style.backgroundColor = "rgb(242,177,121)"; 
                 break;
-            case 8:
-                blockAt(x,y).style.backgroundColor = "rgb(242,177,121)"
+            case 8: blockAt(x,y).style.backgroundColor = "rgb(245,149,99)"; 
                 break;
-            case 16:
-                blockAt(x,y).style.backgroundColor = "rgb(245,149,99)"
+            case 16: blockAt(x,y).style.backgroundColor = "rgb(246, 124, 95)"; 
                 break;
-            case 32:
-                blockAt(x,y).style.backgroundColor = "rgb(246, 124, 95)"
+            case 32: blockAt(x,y).style.backgroundColor = "rgb(246, 94, 59)"; 
                 break;
-            case 64:
-                blockAt(x,y).style.backgroundColor = "rgb(246, 94, 59)"    
+            case 64: blockAt(x,y).style.backgroundColor = "rgb(237, 207, 114)"; 
                 break;
-            case 128:
-                blockAt(x,y).style.backgroundColor = "rgb(237, 207, 114)"
+            case 128: blockAt(x,y).style.backgroundColor = "rgb(237, 204, 97)"; 
                 break;
-            case 256:
-                blockAt(x,y).style.backgroundColor = "rgb(237, 204, 97)"
+            case 256: blockAt(x,y).style.backgroundColor = "rgb(237, 200, 80)"; 
                 break;
-            case 512:
-                blockAt(x,y).style.backgroundColor = "rgb(237, 200, 80)"
+            case 512: blockAt(x,y).style.backgroundColor = "rgb(237, 200, 80)"; 
                 break;
-            case 1024:
-                blockAt(x,y).style.backgroundColor = "rgb(237, 197, 63)"
+            case 1024: blockAt(x,y).style.backgroundColor = "rgb(237, 197, 63)"; 
                 break;
-            case 2048:
-                blockAt(x,y).style.backgroundColor = "rgb(237, 194, 46)"
+            case 2048: blockAt(x,y).style.backgroundColor =  "rgb(237, 194, 46)"; 
                 break;
-            default:
-                blockAt(x,y).style.backgroundColor = "black"
+            default: blockAt(x,y).style.backgroundColor = 'black'; 
+                break;
         }
-
     }
 }
 
@@ -74,11 +61,11 @@ function powerOf2(num) {
     return j
 }
 
-function evalScore(gameState) {
+function evalScore(state) {
     total = 0
     for (i = 0 ; i < 16 ; i++) {
-        if (gameState[i] != "") {
-            total += gameState[i]*(powerOf2(gameState[i])-1)
+        if (state[i] != "") {
+            total += state[i]*(powerOf2(state[i])-1)
         }
     }
     return total
@@ -97,7 +84,7 @@ function updateScore() {
 
 function delBlock(x,y) {
     blockAt(x,y).innerHTML = ""
-    blockAt(x,y).value = ""
+    blockAt(x,y).value = 0
     blockAt(x,y).style.backgroundColor = ""
 }
 
@@ -105,184 +92,12 @@ function blockAt(x,y) {
     return document.getElementById(convertCoords(x,y))
 }
 
-function moveBlock(x1,y1,x2,y2) {
-    block1 = blockAt(x1,y1)
-    block2 = blockAt(x2,y2)
-
-    block2.style.backgroundColor = block1.style.backgroundColor
-    block2.value = block1.value
-    block2.innerHTML = block1.innerHTML
-
-    delBlock(x1,y1)
-}
-
-// falta if innerHTML != ""
-function move(dir) {
-
-    var state = gameState()
-
-    switch (dir) {
-        case "up": {
-
-            for (i=1;i<=4;i++){
-                for (j=1;j<=4;j++){
-                    
-                    if (blockAt(i,j).innerHTML != "") {
-
-                        var inc = 0
-                        while (i-inc > 0){
-                            if (i-inc-1 > 0 && blockAt(i-inc-1,j).innerHTML == "") {
-                                inc++
-                                continue
-                            }
-                            else if (i-inc-1 > 0 && blockAt(i-inc-1,j).innerHTML == blockAt(i,j).innerHTML){
-                                inc++
-                                break
-                            }
-                            else {
-                                break
-                            }
-    
-                        }
-    
-                        if (inc != 0) {
-    
-                            if (blockAt(i-inc,j).innerHTML == ""){
-                                moveBlock(i,j,i-inc,j)
-                            }
-                            else if (blockAt(i-inc,j).innerHTML == blockAt(i,j).innerHTML){
-                                newBlock(parseInt(blockAt(i,j).value)*2,i-inc,j)
-    
-                                delBlock(i,j)
-                            }
-                        }  
-                    }
-                }
-            }
-            break;
-        }
-
-        case "down":{
-
-            for (i=4;i>=1;i--) {
-                for (j=4;j>=1;j--) {
-
-                    if (blockAt(i,j).innerHTML != ""){
-            
-                        var inc = 0
-                        while (i+inc < 5){
-                            if (i+inc+1 < 5 && blockAt(i+inc+1,j).innerHTML == "") {
-                                inc++
-                            }
-                            else if (i+inc+1 < 5 && blockAt(i+inc+1,j).innerHTML == blockAt(i,j).innerHTML){
-                                inc++
-                                break
-                            }
-                            else {
-                                break
-                            }
-                        }
-            
-                        if (inc != 0) {
-            
-                            if (blockAt(i+inc,j).innerHTML == ""){
-                                moveBlock(i,j,i+inc,j)
-                            }
-                            else if (blockAt(i+inc,j).innerHTML == blockAt(i,j).innerHTML){
-                                newBlock(parseInt(blockAt(i,j).value)*2,i+inc,j)
-                                delBlock(i,j)
-                            }
-                        }  
-                    }
-                }
-            }
-            break;
-        }
-
-        case "right":{
-            for (j=4;j>=1;j--) {
-                for (i=4;i>=1;i--) {
-                    if (blockAt(i,j).innerHTML != ""){
-            
-                        var inc = 0
-                        while (j+inc < 5){
-                            if (j+inc+1 < 5 && blockAt(i,j+inc+1).innerHTML == "") {
-                                inc++
-                            }
-                            else if (j+inc+1 < 5 && blockAt(i,j+inc+1).innerHTML == blockAt(i,j).innerHTML){
-                                inc++
-                                break
-                            }
-                            else {
-                                break
-                            }
-                        }
-            
-                        if (inc != 0) {
-            
-                            if (blockAt(i,j+inc).innerHTML == ""){
-                                moveBlock(i,j,i,j+inc)
-                            }
-                            else if (blockAt(i,j+inc).innerHTML == blockAt(i,j).innerHTML){
-                                newBlock(parseInt(blockAt(i,j).value)*2,i,j+inc)
-                                delBlock(i,j)
-                            }
-                        }  
-                    }
-                }
-            }
-            break;
-        }
-
-        case "left":{
-            for (j=1;j<=4;j++) {
-                for (i=1;i<=4;i++) {
-
-                    if (blockAt(i,j).innerHTML != ""){
-            
-                        var inc = 0
-                        while (j-inc > 0){
-                            if (j-inc-1 > 0 && blockAt(i,j-inc-1).innerHTML == "") {
-                                inc++
-                            }
-                            else if (j-inc-1 > 0 && blockAt(i,j-inc-1).innerHTML == blockAt(i,j).innerHTML){
-                                inc++
-                                break
-                            }
-                            else {
-                                break
-                            }
-                        }
-            
-                        if (inc != 0) {
-            
-                            if (blockAt(i,j-inc).innerHTML == ""){
-                                moveBlock(i,j,i,j-inc)
-                            }
-                            else if (blockAt(i,j-inc).innerHTML == blockAt(i,j).innerHTML){
-                                newBlock(parseInt(blockAt(i,j).value)*2,i,j-inc)
-                                delBlock(i,j)
-                            }
-                        }  
-                    }
-                }
-            }
-            break;
-        }
-    }
-
-    if (state.toString() != gameState().toString()){
-        newBlock()
-    }
-
-}
-
 function gameState(){
     var gameBoard = []
     for (i=1;i<=4;i++){
         for (j=1;j<=4;j++){
-            if (blockAt(i,j).value == undefined) {
-                gameBoard.push("")
+            if (blockAt(i,j).value == 0) {
+                gameBoard.push(0)
             }
             else{
                 gameBoard.push(blockAt(i,j).value)  
@@ -298,8 +113,8 @@ function loadGame(gameState) {
 
     var index = 0
     for (i=1;i<=4;i++){
-        for (j=1;j<=4;j++){
-            if (gameState[index] != "") 
+        for (j=1;j<=4;j++){ 
+            if (gameState[index] != 0)
                 newBlock(gameState[index],i,j)
             index++
         }
@@ -319,69 +134,170 @@ function clearGame() {
 function newGame() {
     
     document.getElementById("endGameScreen").style.display = "none"
+    prevGameStates = []
 
     clearGame()
     newBlock()
     updateScore()
 }
 
-function evalEnd() {
-    var state = gameState()
-    
-    move("up")
-    move("down")
-    move("left")
-    move("right")
+function evalEnd(state) {
+    dirs = ['u','d','r','l']
 
-    if (state.toString() == gameState().toString()) {
-        return true
-    }
-    else {
-        loadGame(state)
-        return false
-    }
+    var retVal = true
+
+    dirs.forEach(dir => {
+        if (state.toString() != moveAlg(state , dir).toString()) { 
+            retVal = false
+        }
+    })
+
+    return retVal
 }
 
-function endGame() {
-    document.getElementById("endGameScreen").style.display = "block"
+function lineShiftAlg(line) {
+
+    /*
+    Returns a line with its elements ahifted to the right 
+    having in mind the rules of the game
+    */
+
+    line = line.map( value => {
+        return value.toString() 
+    })
+
+    for (i = 2 ; i >= 0 ; i--) {
+
+        if (line[i] != "0") {
+            
+            for (inc = 0 ; inc <= 3 ; inc++ ) {
+                
+                if (line[i+inc+1] == "0") {
+                    if (i+inc+1 == 3) {
+                        line[i+inc+1] = line[i] 
+                        line[i] = "0"
+                        break    
+                    }
+                }
+
+                else if (line[i+inc+1] == line[i]) {
+                    line[i+inc+1] = parseInt(line[i+inc+1])*2 + "!"
+                    line[i] = "0"
+                    break
+                }
+
+                else if (line[i+inc+1] != line[i] || line[i+inc+1].endsWith("!")) {
+                    if (inc != 0) {
+                        line[i+inc] = line[i] 
+                        line[i] = "0"
+                    }
+                    break
+                }
+                
+            }
+        }
+
+    }
+
+    line = line.map( value => {
+        return parseInt(value.replace("!",""))
+    })
+
+    return line
+}
+
+function moveAlg(state,dir) {
+
+    var lines = []
+    for (i=0 ; i<4 ; i++) {
+        switch (dir) {
+            case 'd':
+                var line = [state[i] , state[4+i] , state[8+i] , state[12+i]]
+                lines.push(line)
+                break;
+
+            case 'u':
+                var line = [state[12+i] , state[8+i] , state[4+i] , state[i]]
+                lines.push(line)
+                break;
+
+            case 'r':
+                var line = [state[0+4*i] , state[1+4*i] , state[2+4*i] , state[3+4*i]]
+                lines.push(line)
+                break;
+
+            case 'l':
+                var line = [state[3+4*i] , state[2+4*i] , state[1+4*i] , state[0+4*i]]
+                lines.push(line)
+                break;
+        }
+        
+    }
+
+    lines = lines.map(value => {
+        return lineShiftAlg(value)
+    })
+
+    var newState = []
+    for (k=0 ; k<4 ; k++) {
+        switch (dir) {
+            case 'd':
+                newState.push(lines[0][k] , lines[1][k] , lines[2][k] , lines[3][k])
+                break;
+
+            case 'u':
+                newState.push(lines[0][3-k] , lines[1][3-k] , lines[2][3-k] , lines[3][3-k])
+                break;
+
+            case 'r':
+                newState.push(lines[k][0] , lines[k][1] , lines[k][2] , lines[k][3])
+                break;
+
+            case 'l':
+                newState.push(lines[k][3] , lines[k][2] , lines[k][1] , lines[k][0])
+                break;
+        }
+        
+    }
+
+    return newState
+}
+
+function move(dir) {
+    state = gameState()
+    newState = moveAlg(state, dir)
+
+    if (state.toString() != newState.toString()) {
+        prevGameStates.push(state)
+        loadGame(newState)
+        newBlock()
+        updateScore()
+
+    }
+
+    if (evalEnd(newState)) {
+        document.getElementById("endGameScreen").style.display = "block"
+    }
+
+    console.log(prevGameStates)
 }
 
 document.addEventListener('keydown', (event) => {
     switch (event.key) {
         case "ArrowLeft":{
-            prevGameState = gameState()
-            move("left")
-            if (evalEnd()) {
-            endGame()
-            }
-            updateScore()
+            move('l')
             break;
         }
         case "ArrowDown":{
-            prevGameState = gameState()
-            move("down")
-            if (evalEnd()) {
-            endGame()
-            }
-            updateScore()
+            move('d')
             break;
         }
         case "ArrowRight":{
-            prevGameState = gameState()
-            move("right")
-            if (evalEnd()) {
-            endGame()
-            }
-            updateScore()
+            move('r')
             break;
         }
         case "ArrowUp":{
-            prevGameState = gameState()
-            move("up")
-            if (evalEnd()) {
-            endGame()
-            }
-            updateScore()
+            move('u')
             break;
         }
         case "r":{
@@ -389,11 +305,9 @@ document.addEventListener('keydown', (event) => {
             break;
         }
         case "Backspace": {
-            loadGame(prevGameState)
-            updateScore()
+            prevGameStates.pop()
+            loadGame(prevGameStates[prevGameStates.length-1])
             break;
         }
     }
-  }, false);
-
-//   O zé disse para fazer melhor
+}, false);
